698283d1b10f53ea7ffa2f8364b53f980f162f4b
alishaevn <alieva.inc@gmail.com>
2018-04-05 09:39:51 -0700

    notated

---
+++ LinkedList.js
@@ -0,0 +1,183 @@
+'use strict';
+
+const _Node = require('./Node');
+
+/* 
+Walk through the Linked list code in the curriculum and understand it well. Then write a linked list class 
+and with its core functions (insertFirst, insertLast, remove, find) from scratch.
+
+
+FICTIONAL LINKED LIST IN MEMORY
+ value:    | {alpha | # | # | {beta | # | {gamma | # | # |
+ next:     |   3}   |   |   |  5}   |   |  null} |   |   |
+ address:      0      1   2    3      4     5      6   7
+
+  - addresses 0, 3 && 5 contain the nodes of the LL
+  - the remaining addresses are used by other things
+ */
+
+ 
+
+/* =======================================================================================
+                        SINGLY LINKED LIST
+======================================================================================= */
+
+class LinkedList {
+  // a linked list is a list of connected nodes
+
+  constructor() {
+    this.head = null;
+    // the head is the location of the first node
+    // if there's no list yet, the location is null, i.e. non existent
+  }
+
+  /* =============================
+  INSERT
+  ============================= */
+
+  insertFirst(value) {
+    // create the first node in the list
+    /* the _Node constructor takes parameters so you can't just instantiate it as a variable outside 
+      of the class */
+
+    this.head = new _Node(value, this.head);
+    // reassign the value of the head to be a new node
+    /*  new _Node(alpha, null) =>
+          node = { 
+                  value: alpha, 
+                  next: null
+          }
+          (next is null because it's both the beginning && end of the list at this moment)
+     */
+  }
+
+
+  insertLast(value) {
+    // add a node to the end of the list
+
+    if (this.head === null) {
+      // if we don't have a head value, then we don't have a list at all
+      // so first we would need to create the list
+      this.insertFirst(value);
+    }
+    else {
+      // if we do have a head, we must start at the head and go through the entire list to find the end
+
+      let currentNode = this.head;
+      // currentNode = { value: alpha, next: 3 }
+
+      while (currentNode.next !== null) {
+        // if next prop was NOT null, that means we have another node in our list
+        // we would reassign the variable currentNode to be the value of the next node and see if the while loop is still true for that node
+        currentNode = currentNode.next;
+
+        // if we get to a point where currentNode.next === null ...we break out of this loop
+      }
+
+      currentNode.next = new _Node(value, null);
+      // once we reach the end of the list, we take that node's next prop and reassign it by creating a new node
+      // new _Node(delta, null) => node { value: delta, next: null }
+    }
+  }
+
+
+
+  /* =============================
+  FIND
+  ============================= */
+
+  find(value) { // value = delta
+
+    // 0: {value: alpha, next: 3}      
+    // 3: {value: beta, next:5}      
+    // 5: {value: gamma, next: null}
+    // 8: {value: delta, next: null}
+
+    let currentNode = this.head;
+    // start at the head
+    // currentNode = {value: alpha, next: 3}
+
+    if (!this.head) {
+      // if the list is empty return null
+      return null;
+    }
+
+    while (currentNode.value !== value) {
+      // remain in this loop as long as the currentNode.value doesn't equal the value of the value we're looking for... 
+      // we're checking to see does the value not exist in this list...
+      // or does it just exist on another node in the list?
+
+      if (currentNode.next === null) {
+        // if curreNode.next is null that means there are no more nodes to check
+   
+        return null;
+        // return null because the value does not exist in this list
+      }
+      else {
+        currentNode = currentNode.next;
+        // otherwise reassign the value of currentNode to the next node and go through the loop again
+
+        // if we get to a point where currentNode.next === value ... we break out of this loop
+      }
+    }
+    
+    return currentNode;
+    // we found the node whose value matches the value we're looking for!
+  }
+
+
+  /* =============================
+  REMOVE
+  ============================= */
+
+  remove(value) { // value = beta
+
+    if (!this.head) {
+      // if the list is empty return null
+      return null;
+    }
+
+    if (this.head === value) {
+      // if the value of the first node is the value we're looking for...
+
+      this.head = this.head.next;
+      // reassign the value of this.head to be the NEXT node in the list
+      // a LL must always have a head && the head must always point to the first node
+      
+      return;
+      // return the updated LL
+    }
+    
+    let currentNode = this.head;
+    // currentNode = {value: alpha, next: 3}
+    
+    let previousNode = this.head;
+    // currentNode = {value: alpha, next: 3}
+
+    while ((currentNode !== null) && (currentNode.value !== value)) {
+      // while we have a node to evaluate AND the current node isn't the one we're looking for...
+       
+      previousNode = currentNode;
+      // reassign previousNode to be the value of currentNode
+
+      currentNode = currentNode.next;
+      // then reassign currentNode to be the value of the next node in the list
+      
+      // if we get to a point where we either have no more nodes to look at (currentNode === null) OR currentNode's value is the value we want (currentNode.value === value)... we break out of this loop
+    }
+
+    if (currentNode === null) {
+      // if currentNode === null, there's nothing to remove because the value being searched for isn't in this LL
+      console.log('Value not found');
+      return;
+    }
+
+    previousNode.next = currentNode.next;
+    // if (currentNode.value === value)...
+    // reassign the next value of the node BEFORE it, to point to the next value of the node AFTER it
+    // if there isn't a node after it, the next value of the node BEFORE it will be "null"
+  }
+
+}
+
+module.exports = LinkedList;
