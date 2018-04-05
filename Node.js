d0246057ff802196961dd8e1dcd1d3b35f3f8d4d
alishaevn <alieva.inc@gmail.com>
2018-04-05 08:05:17 -0700

    notated

---
+++ Node.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/* =============================
+SINGLY LINKED NODE
+============================= */
+// the node class is private && is intended to only be used by the LinkedList class
+
+class _Node {
+  // these nodes are an object with 2 props
+  // the value of next is the memory address of the next node in the list
+  /* let node = {
+          value: 'alpha',
+          next: 11
+         }
+   */
+
+  constructor(value, next) {
+    this.value = value,
+    this.next = next;
+  }
+}
+
+module.exports = _Node;
+
+
+
+/* =============================
+DOUBLY LINKED NODE
+============================= */
+
+class _DoubleNode {
+  // these nodes are an object with 3 props
+  // the value of previous is the memory address of the previous node in the list
+  /* let node = {
+          value: 'alpha',
+          next: 11,
+          previous: 4
+         }
+   */
+
+  constructor(value, next, previous) {
+    this.value = value,
+    this.next = next;
+    this.previous = previous;
+  }
+}
+
+module.exports = _DoubleNode;
